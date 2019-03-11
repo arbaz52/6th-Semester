a2f59e55e76188bf08ed12387330efa2ae0e8850
arbaz52 <arbaz5256@gmail.com>
2019-03-11 17:03:56 +0500

    ADDED FACIAL DETECTION USING OPENCV2 YAYYYYYYY

---
+++ Computer Vision/Face Detection/main.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 16:10:17 2019
+
+@author: Shark
+"""
+
+import cv2
+
+faceFile = "C:\ProgramData\Anaconda3\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(faceFile)
+videoCapture = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = videoCapture.read()
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray, 
+                                         scaleFactor = 1.1, 
+                                         minNeighbors = 5, 
+                                         minSize = (30, 30), 
+                                         flags = cv2.CASCADE_SCALE_IMAGE)
+    print("faces: ", len(faces))
+    for i in range(len(faces)):
+        (x, y, w, h) = faces[i]
+        cv2.rectangle(gray, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.imwrite("face_" + str(i) + ".png", gray[y: y+h+1, x: x+w+1])
+    
+    cv2.imshow("frame", gray)
+    cv2.imwrite("faces.png", gray)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+
+videoCapture.release()
+cv2.destroyAllWindows()
